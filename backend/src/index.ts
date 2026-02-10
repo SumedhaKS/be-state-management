60476fe5ea59f2733c25dcd6e160e2a198599862
SumedhaKS <kssumedha24@gmail.com>
2026-02-10 16:03:47 +0530

    uses the class-approach

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,9 @@
-import { games } from "./store";
+import { gameManager } from "./store";
 import { startLogger } from "./logger";
 
 startLogger();
 
 setInterval(() => {
-    games.push({
-        id: Math.random().toString(),
-        whitePlayer: 'whity',
-        blackPlayer: 'blacky',
-        moves: [],
-    })
-}, 5000)
+    gameManager.addGame(Math.random().toString())
+}, 5000);
+
